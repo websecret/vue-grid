bf84457b6569f481a899ba5454f0cc5f12ccd3be
blax <blax56@gmail.com>
2019-06-26 16:14:20 +0300

    init docs

---
+++ .vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    title: 'Vue simple Grid System',
+    dest: 'docs'
+}
---
+++ docs/assets/js/10.3fa139c5.js
@@ -0,0 +1,4 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [10],
+  { 9: function(n, w, o) {} }
+]);
